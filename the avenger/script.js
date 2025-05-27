e347ad9927a2218b21f080f78be3787a071ac90c
Persingby <gabriely.persin.silva@escola.pr.gov.br>
2025-05-27 11:18:07 -0300

    Add files via upload

---
+++ the avenger/script.js
@@ -0,0 +1,10 @@
+// Rolagem suave para links do menu
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const destino = document.querySelector(this.getAttribute('href'));
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
